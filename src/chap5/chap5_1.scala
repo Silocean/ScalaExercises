7832639760edc719b597f9b2c1af7a8175dbfb52
Silocean <silenceocean@live.com>
2015-12-08 16:05:33 +0800

    update chap5_1

--- src/chap5/chap5_1.scala
+++ src/chap5/chap5_1.scala
@@ -3,15 +3,22 @@ package chap5
 /**
  * Created by Tracy on 2015-12-08.
  */
-class chap5_1 {
-    private var value = 0
+object chap5_1 {
+    def main(args: Array[String]) {
 
-    def increment() {
-        if (value + 1 < Int.MaxValue)
-            value += 1
-        else
-            value = 0
     }
 
-    def current = value
+    class Counter {
+        private var value = 0
+
+        def increment() {
+            if (value + 1 < Int.MaxValue)
+                value += 1
+            else
+                value = 0
+        }
+
+        def current = value
+    }
+
 }
