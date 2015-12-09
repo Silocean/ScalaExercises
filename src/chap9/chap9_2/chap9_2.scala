4eb291e68c8f116850869d7510bf76dfdc6d4848
Silocean <silenceocean@live.com>
2015-12-09 20:30:13 +0800

    update chap9_2

--- src/chap9/chap9_2/chap9_2.scala
+++ src/chap9/chap9_2/chap9_2.scala
@@ -6,6 +6,7 @@ import scala.io.Source
 
 /**
   * Created by Silocean on 2015-12-09.
+  * 读取文件内容，将Tab键替换成空格
   */
 object chap9_2 extends App {
     val source = Source.fromFile("./src/chap9/chap9_2/source.txt")
