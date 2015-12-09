516c1874dc30c5039147c8553b7842f932fff07c
Silocean <silenceocean@live.com>
2015-12-09 20:22:52 +0800

    add chap9_2

---
+++ src/chap9/chap9_2/chap9_2.scala
@@ -0,0 +1,17 @@
+package chap9.chap9_2
+
+import java.io.PrintWriter
+
+import scala.io.Source
+
+/**
+  * Created by Silocean on 2015-12-09.
+  */
+object chap9_2 extends App {
+    val source = Source.fromFile("./src/chap9/chap9_2/source.txt")
+    val out = new PrintWriter("./src/chap9/chap9_2/result.txt")
+    for (line <- source.getLines().toArray) {
+        out.println(line.replaceAll("\t", " "))
+    }
+    out.close()
+}
