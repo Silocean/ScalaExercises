eec8487571f96069a539f51850fe67ded0b28001
Silocean <silenceocean@live.com>
2015-12-09 09:30:36 +0800

    finish chap6_5

---
+++ src/chap6_5/PrintArgs.scala
@@ -0,0 +1,12 @@
+package chap6_5
+
+/**
+ * Created by Tracy on 2015-12-09.
+ * 编写一个Scala应用程序,使用App特质,以反序打印命令行参数,用空格隔开。
+ * 举例来说,scala Reverse Hello World应该打印World Hello
+ */
+object PrintArgs extends App{
+    val result = for (x <- args reverse) yield x
+    println(result.mkString(" "))
+}
+
