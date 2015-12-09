ec59ccd216c753d7adecf290214bef0e780cc5e8
Silocean <silenceocean@live.com>
2015-12-09 08:48:33 +0800

    update chap6_3

--- src/chap6/chap6_3/Origin.scala
+++ src/chap6/chap6_3/Origin.scala
@@ -4,12 +4,13 @@ import java.awt.Point
 
 /**
  * Created by Tracy on 2015-12-09.
+ * 定义一个扩展自java.awt.Point的Origin对象。为什么说这实际上不是个好主意？(仔细看Point类的方法)  
+ *
+ * Point中的getLocation方法返回的是Point对象，如果想返回Origin对象，需要Origin类才行
  */
 object Origin extends java.awt.Point with App {
     override def getLocation: Point = super.getLocation
 
-    Origin.move(2, 6)
-
-    println(Origin.toString)
+    println(Origin.getLocation)
 
 }
