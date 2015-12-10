5eb350d629e6f6eefe500e5d02431f43e869bd55
Silocean <silenceocean@live.com>
2015-12-10 13:14:54 +0800

    update

--- src/RegexScala.scala
+++ src/RegexScala.scala
@@ -3,7 +3,7 @@
   */
 object RegexScala extends App {
     val itemsPattern = "([0-9]+) ([a-z]+)".r
-    val itemsPattern(num, item) = "2 bles"
+    // val itemsPattern(num, item) = "2 bles"
     for (itemsPattern(num, item) <- itemsPattern.findAllIn("99 bottles, 98 bottles"))
         println(num + " " + item)
 
