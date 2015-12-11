a36c21a4cc6ce920e9067b53143d4574bedb767d
Silocean <silenceocean@live.com>
2015-12-11 16:43:38 +0800

    delete chap11_3

---
+++ src/chap11/chap11_3/Test.scala
@@ -0,0 +1,11 @@
+package chap11.chap11_3
+
+/**
+  * Created by Silocean on 2015-12-11.
+  */
+object Test extends App {
+    val f = new Fraction(2, 3)
+    val b = new Fraction(4, -5)
+    println(f + b)
+    println(f / b)
+}
