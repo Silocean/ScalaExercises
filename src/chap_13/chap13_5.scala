0a06113ab5efaff7ee16be9a10304241b4ab5220
Yunfei Si <silenceocean@live.com>
2016-01-15 18:58:12 +0800

    update chap_13_5

--- src/chap_13/chap13_5.scala
+++ src/chap_13/chap13_5.scala
@@ -4,9 +4,17 @@ package chap_13
   * Created by Silocean on 2015-12-17.
   */
 object chap13_5 extends App {
-    def mkString(list: List[String]) = {
-        list.reduceLeft(_ + _)
+    var tag = ","
+
+    def mkString(list: List[String], t: String) = {
+        tag = t
+        list.reduceLeft((a, b) => addString(a, b))
+        // list.reduceLeft((_, _) => addString(_, _)) // Error:missing parameter type for expanded function...
     }
 
-    println(mkString(List("1", "2", "4")))
+    def addString(s1: String, s2: String) = {
+        s1 + tag + s2
+    }
+
+    println(mkString(List("1", "2", "4"), ";"))
 }
